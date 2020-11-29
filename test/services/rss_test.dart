cada46b98124e60009bbfeb7b56bfccf95a487c8
Matias de Andrea <deandreamatias@gmail.com>
2020-11-29 15:18:35 +0100

    test: Update test description

--- test/services/rss_test.dart
+++ test/services/rss_test.dart
@@ -1,11 +1,9 @@
-import 'dart:math';
-
 import 'package:notifications/services/rss_service.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Rss service -', () {
-    test('Test ', () async {
+    test('Get last episode if is wednesday ', () async {
       final rssService = RssService();
       final episode = await rssService.getLastEpisode();
 
